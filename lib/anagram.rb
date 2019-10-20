cf573304b37de25177093bc48bd4579787389a43
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-10-20 04:55:41 +0000

    Automatically backed up by Learn

---
+++ lib/anagram.rb
@@ -0,0 +1,18 @@
+class Anagram
+  attr_accessor :word 
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def mathc(array_of_words)
+    return_array = []
+    array_of_words. each do |element|
+      (@word.split(" ").sort) == (element.split(" ").sort)
+      return_array << element
+    end
+  end
+  return_array
+end
+
+listen
