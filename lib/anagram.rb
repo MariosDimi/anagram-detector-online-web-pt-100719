51379808f53db9b90a85fa0f4915a70acd4e2809
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-10-20 04:49:43 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,4 +7,12 @@ class Anagram
   end
   
   def match(array_of_words)
+    (@word.split("").sort) == (element.split ("").sort)
+      return_array << element
+    end
+    return_array
+  end
 end
+
+listen = Anagram.new("listen")
+listen.match(%w(enlist google inlets banana))
