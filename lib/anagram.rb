da755319e623c2897c25efd1f43130cb5a6cb98c
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-10-20 04:50:39 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,18 +1,23 @@
-class Anagram
-  
-  attr_accessor :word 
-  
-  def initialize(word)
-    @word = word
-  end
-  
-  def match(array_of_words)
-    (@word.split("").sort) == (element.split ("").sort)
-      return_array << element
-    end
-    return_array
-  end
-end
 
-listen = Anagram.new("listen")
-listen.match(%w(enlist google inlets banana))
+
+
+
+
+
+
+#class Anagram
+ # attr_accessor :word 
+  #def initialize(word)
+   # @word = word
+  #end
+  
+  #def match(array_of_words)
+  #  (@word.split("").sort) == (element.split ("").sort)
+      return_array << element
+   # end
+  #  return_array
+#  end
+#end
+
+#listen = Anagram.new("listen")
+#listen.match(%w(enlist google inlets banana))
